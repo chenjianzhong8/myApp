0f0170815eeed99f422eabf97ea3e9b826fc0a76
jason <jason_hzcxyd@sina.com>
2019-03-10 22:28:00 +0800

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+N
+my-app/src/view/index/index.js,3\3\3383e3b6be43755ba150668f15c818482531b5c0
---
+++ my-app/src/view/detail/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react"
+class Detail extends Component{
+    render() {
+        return "文章详情"
+
+    }
+}
+export default Detail;
